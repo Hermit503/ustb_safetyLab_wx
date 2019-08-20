b6a4ed3fc387bc22ac5ccaacc0b7ee34b1817f7d
ljbjkjdxtj <2601454425@qq.com>
2019-08-20 22:41:59 +0800

    修改跳转页面

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ App({
   },
   globalData: {
     userInfo: null,
+    msg: null,//cu-tag badge
   },
   onLaunch: function () {
     wx.getSystemInfo({
@@ -44,6 +45,7 @@ App({
         this.globalData.Custom = custom;
         this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
         this.globalData.Url = 'http://saftylab.test/api/v1';
+        this.globalData.Domain = 'http://saftylab.test';
         // this.globalData.Url = 'https://www.pipicui.wang/api/v1';
         // this.globalData.Url = 'http://www.hlhs.xyz/api/v1';
         // this.globalData.Url = 'http://119.23.243.176/api/v1';
