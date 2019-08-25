d51db71429bdfe0009557d3f8420847e3ec63b87
ljbjkjdxtj <2601454425@qq.com>
2019-08-25 15:14:21 +0800

    消息列表及详情

--- pages/function/notice/message.js
+++ pages/function/notice/message.js
@@ -5,14 +5,142 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabList:['最近消息','历史消息'],
+    messageList: {
+      0: [{
+        "name": "John",
+        "main": "DoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoe",
+        "id": 1,
+        "type":"notice"
+      }],
+      1: [{
+        "name": "John",
+        "main": "DoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoeDoe",
+        "id": 2,
+        "type":"chemical"
+      }]
+    },
+    winWidth: 0,
+    winHeight: 0,
+    // tab切换  
+    currentTab: 0,
+    startDate: '2018-12',
+    endDate: '2019-12',
+    end:'',//结束日期
+    // TabCur: 0,
+    // scrollLeft: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this;
+    var timestamp = Date.parse(new Date());
+    var date = new Date(timestamp);
+    //获取年份  
+    var Y = date.getFullYear();
+    //获取月份  
+    var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
+    var date = Y + '-' + M; 
+    that.setData({
+      endDate: date,
+      end: date
+    });
+     wx.getSystemInfo({
 
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+
+    });  
+  },
+
+  getOneMessage(e) {
+    wx.navigateTo({
+      url: 'messageDetail?id=' + e.currentTarget.dataset.id + '&type=' + e.currentTarget.dataset.type,
+      success: function (res) {
+
+      }
+    })
+  },
+
+  // ListTouch触摸开始
+  ListTouchStart(e) {
+    this.setData({
+      ListTouchStart: e.touches[0].pageX
+    })
+  },
+
+  // ListTouch计算方向
+  ListTouchMove(e) {
+    this.setData({
+      ListTouchDirection: e.touches[0].pageX - this.data.ListTouchStart > 0 ? 'right' : 'left'
+    })
+  },
+
+  // ListTouch计算滚动
+  ListTouchEnd(e) {
+    if (this.data.ListTouchDirection == 'left') {
+      this.setData({
+        modalName: e.currentTarget.dataset.target
+      })
+    } else {
+      this.setData({
+        modalName: null
+      })
+    }
+    this.setData({
+      ListTouchDirection: null
+    })
+  },
+  exit: function () {
+    wx.navigateTo({
+      url: './pages/index/index',
+    })
+  },
+
+  tabSelect(e) {
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+      scrollLeft: (e.currentTarget.dataset.id - 1) * 60
+    })
+  },
+
+  /** 
+    * 滑动切换tab 
+    */
+  bindChange: function (e) {
+    var that = this;
+    that.setData({ currentTab: e.detail.current });
+  },
+  /** 
+   * 点击tab切换 
+   */
+  swichNav: function (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
+  },
+
+  StartDateChange(e) {
+    this.setData({
+      startDate: e.detail.value
+    })
+  },
+
+  EndDateChange(e) {
+    this.setData({
+      endDate: e.detail.value
+    })
   },
 
   /**
